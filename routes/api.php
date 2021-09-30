465c15a0dc7ccf6f0c7e8b25c9c5bfd68ceb4fff
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-09-30 09:07:30 +0545

    feat: add api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/blogs', function () {
+    return Http::get('https://blogs.iu.edu/bfca/wp-json/wp/v2/posts?per_page=4')->json();
+});
